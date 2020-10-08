9457c357a609c75429fc53e3d774d26b3f714c5a
Andrea Stagi <stagi.andrea@gmail.com>
2020-10-08 19:26:45 +0200

    Add file watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
-const { parallel, src, dest } = require('gulp');
+const { parallel, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
+const jsFiles = 'js/**/*.js'
+const scssFiles = './scss/**/*.scss'
 
 function javascript(cb) {
-  return src('js/**/*.js')
+  return src(jsFiles)
     .pipe(uglify())
     .pipe(dest('dist'))
 }
 
 function css(cb) {
-  return src('./scss/**/*.scss')
+  return src(scssFiles)
     .pipe(sass({outputStyle: 'compressed'})) // Using gulp-sass
     .pipe(dest('dist'))
 }
 
 exports.build = parallel(javascript, css);
+exports.watch = function () {
+  watch(jsFiles, javascript);
+  watch(scssFiles, css);
+};
